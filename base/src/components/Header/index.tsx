df0bf2816a75da5085e12374cde31c6f6aee296a
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-03-03 18:27:04 -0300

    feat: criado o header

--- base/src/components/Header/index.tsx
+++ base/src/components/Header/index.tsx
@@ -1,10 +1,22 @@
 import './styles.css'
 import arrow from '../../assets/arrow-back.svg'
+import logo from '../../assets/logo.svg'
+import { useNavigate } from 'react-router-dom'
 
-function Header (){
+type Props = {
+  showArrowBack?: boolean
+}
+
+function Header (showArrowBack: Props){
+  const navigate = useNavigate()
+
   return(
     <header>
-
+      { showArrowBack &&
+        <img src={arrow} alt="arrow back" className='arrow-back'
+        onClick={()=> navigate(-1)}
+        />}
+      <img src={logo} alt="logo" />
     </header>
   )
 }
--- base/src/pages/Main/index.tsx
+++ base/src/pages/Main/index.tsx
@@ -1,9 +1,10 @@
 import './styles.css';
+import Header from '../../components/Header';
 
 function Main() {
   return (
     <div className='container'>
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
